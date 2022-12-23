2d376d77df99ee871207b39b9470a1dc42eb1f14
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2022-12-22 22:31:54 -0800

    add imgs and admin paragraph content

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
+from django import forms
 from .models import BlogPost
 
-admin.site.register(BlogPost)
+class BlogPostAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=forms.Textarea)
+
+    class Meta:
+        model = BlogPost
+        fields = '__all__'
+
+class BlogPostAdmin(admin.ModelAdmin):
+    form = BlogPostAdminForm
+
+admin.site.register(BlogPost, BlogPostAdmin)
--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -6,3 +8,5 @@ urlpatterns = [
     path('blog/<int:pk>/', views.blog_post, name='blog_post'),
     path('blog/cards/', views.blog_cards, name='blog_cards'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
