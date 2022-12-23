5d7dbc27febb571c1a433ab30baee4b9cb7927d1
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2022-12-22 18:50:18 -0800

    render blog cards and blog post

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('blog/<int:pk>/', views.blog_post, name='blog_post'),
+    path('blog/cards/', views.blog_cards, name='blog_cards'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import BlogPost
 
 # Create your views here.
 def index(request):
   return render(request, 'blog/index.html')
+
+def blog_post(request, pk):
+    post = get_object_or_404(BlogPost, pk=pk)
+    return render(request, 'blog/blog_post.html', {'post': post})
+
+def blog_cards(request):
+    blog_posts = BlogPost.objects.all()
+    return render(request, 'blog/blog_cards.html', {'blog_posts': blog_posts})
