321ac92d57174ed1fb801d37d4bca6919bc3e87d
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-01-05 19:26:00 -0800

    add and style form

---
+++ blog/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class SubscribeForm(forms.Form):
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control'}))
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 from .models import BlogPost
+from .forms import SubscribeForm
 
 # Create your views here.
 def index(request):
@@ -8,8 +11,31 @@ def index(request):
 def about(request):
   return render(request, 'blog/about.html')
 
+# def subscribe(request):
+#   return render(request, 'blog/subscribe.html')
+
 def subscribe(request):
-  return render(request, 'blog/subscribe.html')
+    if request.method == 'POST':
+        form = SubscribeForm(request.POST)
+
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            content = form.cleaned_data['content']
+
+            html = render_to_string('base/emails/contactform.html', {
+                'name':name,
+                'email':email,
+                'content':content
+            })
+
+            send_mail('The contact form subject', 'This is the message', 'noreply@code.com', ['cesarjaramillodev@gmail.com'], html_message=html)
+
+            return redirect('/thankyou/')
+    else:
+        form = SubscribeForm()
+    
+    return render(request, 'blog/subscribe.html', {'form':form})
 
 def blog_post(request, pk):
     post = get_object_or_404(BlogPost, pk=pk)
