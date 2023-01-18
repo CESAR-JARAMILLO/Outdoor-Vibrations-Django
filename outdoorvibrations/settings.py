2238f5752150e68ae53bb46d822f99ae8c0564e9
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-01-17 20:37:52 -0800

    configured for postgresql

--- outdoorvibrations/settings.py
+++ outdoorvibrations/settings.py
@@ -86,10 +86,21 @@ WSGI_APPLICATION = 'outdoorvibrations.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'outdoorvibrations',
+        'USER': 'postgres',
+        'PASSWORD': 'Hikelife89!',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
